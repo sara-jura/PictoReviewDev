b049dc8d613ca8e119c6b559e3cf1ab12e3b0231
sara <jurs02@vse.cz>
2020-07-24 10:41:04 +0200

    RDF generation change

--- mappings/utils.py
+++ mappings/utils.py
@@ -17,14 +17,15 @@ def createGraph(formsets,form_id):
                 cd = f.cleaned_data
                 fieldNode = new[cd.get("field_name").title().replace(" ", "")]
                 mappingNode = new[cd.get("field_name").title().replace(" ", "") + "2" + f.metric.replace(" ", "")]
-                g.add((fieldNode, RDF.type, picr.ReviewFormCriterion))
+                g.add((fieldNode, RDF.type, picr.ReviewFormField))
                 g.add((fieldNode, picr.weight, Literal(cd.get("field_weight"))))
                 g.add((fieldNode, picr.minVal, Literal(cd.get("field_min"))))
+                g.add((fieldNode,picr.fieldOf,new['']))
                 g.add((fieldNode, picr.maxVal, Literal(cd.get("field_max"))))
                 g.add((fieldNode, RDFS.label, Literal(cd.get("field_name"))))
                 g.add((mappingNode, RDF.type, picr.F2M_Mapping))
                 g.add((mappingNode, picr['fieldOfMapping'], fieldNode))
                 g.add((mappingNode, picr.metricOfMapping, papeval[f.metric.replace(" ", "")]))
-                g.add((mappingNode, picr.mappingOf, new['']))
+                #g.add((mappingNode, picr.mappingOf, new['']))
     g.add((new[''],RDF.type,picr.ReviewForm))
     return g
